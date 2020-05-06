12b41b2a6b9f25554a954fb53a76a9a46dd6477b
delphinedara <delphine.dara@gmail.com>
2020-05-05 21:39:31 -0500

    added hard and easy functionalities, visual feedback to the player

--- script.js
+++ script.js
@@ -3,33 +3,38 @@ var userGuessLog =[];
 var attempts = 0;
 var maxGuesses =10;
 
+function gameEnded(){
+  document.getElementById('newGameButton').style.display = 'inline';
+  document.getElementById('easyButton').style.display= 'none';
+  document.getElementById('hardButton').style.display ='none';
+  document.getElementById('inputBox').setAttribute('readonly' , 'readonly');
+}
+
 function easyMode(){
   maxGuesses=10;
+  document.getElementById('easyButton').className= 'activeButton';
+  document.getElementById('hardButton'.className ='');
 }
 
 function hardMode(){
   maxGuesses=5;
+  document.getElementById('hardButton').className= 'activeButton';
+  document.getElementById('easyButton'.className ='');
 }
 
-//NewGame Clicked - Reload the page and start a new game with a new random number
 function newGame() {
   window.location.reload();
 }
 
-//computer picks  a random number
+
 function init() {
   computerGuess= Math.floor (Math.random() * 100 + 1);
-  // console.log(computerGuess);
   document.getElementById('newGameButton').style.display = 'none';
 }
 
-//user input and compare guess
 function compareGuess() {
   var userGuess =" " + document.getElementById('inputBox').value;
-  // console.log(userGuess);
-
   userGuessLog.push(userGuess);
-  // console.log(userGuessLog);
   document.getElementById('guessLog').innerHTML = userGuessLog;
   attempts ++;
   document.getElementById('attempts').innerHTML = attempts;
@@ -43,17 +48,24 @@ function compareGuess() {
       document.getElementById('textOutput').innerHTML="Your guess is too low";
       document.getElementById('inputBox').value="";
       } else{
-      document.getElementById('textOutput').innerHTML="Correct!";
-      document.getElementById('inputBox').value="";
+      document.getElementById('textOutput').innerHTML="Correct! You got it in" + attempts + " attempts.";
+      document.getElementById('container').style.backgroundColor = 'green';
+      gameEnded();
      }
 
   } else {
     if (userGuess > computerGuess){
-      document.getElementById('textOutput').innerHTML = 'Reached maximum attempts, try again!';
+      document.getElementById('textOutput').innerHTML = 'Reached maximum attempts, try again!' + "<br> The number was " + computerGuess;
+      document.getElementById('container').style.backgroundColor = '#e82c4e';
+      gameEnded();
     } else if (userGuess < computerGuess){
-      document.getElementById('textOutput').innerHTML = "Reached maximum attempts, try again!";
+      document.getElementById('textOutput').innerHTML = "Reached maximum attempts, try again!" + "<br> The number was " + computerGuess;
+      document.getElementById('container').style.backgroundColor = '#e82c4e';
+      gameEnded();
     } else {
-      document.getElementById('textOutput').innerHTML = "Correct!";
+      document.getElementById('textOutput').innerHTML = "Correct! You got it in" + attempts + " attempts.";
+      document.getElementById('container').style.backgroundColor = 'green';
+      gameEnded();
     }
   }
   
